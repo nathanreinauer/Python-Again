353b3fdb614359a7972771eb1746758195f3f541
Nathan Reinauer <nathanreinauer@gmail.com>
2016-04-27 22:50:27 -0700

    Finished! Ha!
    Successfully copies NEW files (modified in the last 24 hours) from one
    folder to another.

--- transferdrill.py
+++ transferdrill.py
@@ -2,50 +2,56 @@ import shutil
 import os.path
 from datetime import *
 
-# number of files in folder A and B
-pathA =  os.path.expanduser('~\Desktop\Folder A')
+# Path to each folder
+pathA =  os.path.expanduser('~\Desktop\Customer Orders')
+pathB =  os.path.expanduser('~\Desktop\Home Office')
+
+# Number of files in Customer Orders
 numA = len([f for f in os.listdir(pathA)if os.path.isfile(os.path.join(pathA, f))])
 
-pathB =  os.path.expanduser('~\Desktop\Folder B')
-numB = len([f for f in os.listdir(pathB)if os.path.isfile(os.path.join(pathB, f))])
+
 
 def main():
     start()
-    
+
 def start():
+    
+    # If there are no files in the folder, let user know
     if numA == 0:
-        print "There are no new files in Customer Orders."
-        exit()
+        print "There are no files in Customer Orders."
+        
+    # Opening prompt
     else:
         choice = raw_input("You currently have "+str(numA)+
-                           " new file(s) in Customer Orders. "
-                           "Would you like to move them to Home Office? y/n: ")
+                           " file(s) in Customer Orders. "
+                           "Would you like to move the new ones to Home Office? y/n: ")
+        
         if choice == "y":
+            
+            # Today - 24 hours = "yesterday"
+            yesterday = datetime.now() - timedelta(days=1)
+            
+            # 
+            def modTime(filePath):
+                t = os.path.getmtime(filePath)
+                return datetime.fromtimestamp(t)
+            
+            # Figure out when each file in Customer Orders was modified
             for file in os.listdir(pathA):
-                shutil.copy((pathA+"//"+file), pathB)
-                print pathA+"//"+file+" successfully copied."
+                mod = modTime(pathA+"//"+file)
+
+                # Copy the files that were modified in the last 24 hours
+                if yesterday < mod:
+                    shutil.copy((pathA+"//"+file), pathB)
+                    print pathA+"//"+file+" successfully copied."
+
+                # Ignore older files
+                elif yesterday > mod:
+                    print "File skipped."
+                    
             print "Operation completed."
+    
         else:
             print "OK, bye."
-            exit()
-            
-yesterday = datetime.now() - timedelta(days=1)
-print yesterday
-
-def modTime(filePath):
-    t = os.path.getmtime(filePath)
-    return datetime.fromtimestamp(t)
-
-for file in os.listdir(pathA):
-    d = modTime(pathA+"//"+file)
-    if yesterday > d:
-        print "yes"
-        shutil.copy((pathA+"//"+file), pathB)
-        print pathA+"//"+file+" successfully copied."
-    if yesterday < d:
-        print "no"
-
-
-
 
 start()
