f2cd23910163b24f797ba4e6e9678610b3bb674e
Nathan Reinauer <nathanreinauer@gmail.com>
2016-04-30 08:26:55 -0700

    Simplified SQLite code and made it grab last record
    If you want the SQLite function getCell() to take parameters again, look
    at previous commit

--- DBtransfer/DBtransfer.py
+++ DBtransfer/DBtransfer.py
@@ -7,24 +7,19 @@ conn = sqlite3.connect('TranDB.db')
 c = conn.cursor()
 
 
-#-----RETRIEVE INFO-----#
+#-----RETRIEVE LATEST RECORD-----#
 
-def getCell(cell):
-    c.execute("SELECT * FROM LastCheck WHERE ID =?", str(cell))
+def getCell():
+    c.execute("SELECT * FROM LastCheck ORDER BY ID DESC LIMIT 1")
     return c.fetchall()
 
-inCell = getCell(2)
-
-print inCell
-
-
-#conn.commit()
+print getCell()
 
 #-----ADD INFO-----#
 
 def insertCell(add):
     c.execute("INSERT INTO LastCheck (DateTime) VALUES (?)", (add,))
 
-insertCell('Test Four')
+insertCell('Test Five')
 
 #conn.commit()
