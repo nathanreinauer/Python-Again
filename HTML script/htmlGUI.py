0dd024b08ef8091d7c1b025d9565957584739849
Nathan Reinauer <nathanreinauer@gmail.com>
2016-04-30 22:41:15 -0700

    Finished!
    Trimmed the fat, changed text colors, adjusted widget placement, etc.

--- HTML script/htmlGUI.py
+++ HTML script/htmlGUI.py
@@ -3,17 +3,6 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 
-headContent = (
-"""
-<html>
-<head>
-<title>Summer Sale</title>
-</head>
-<body>
-<h1>
-"""
-)
-
 class GUIhtml:
 
 #-------------------------GUI-------------------------#
@@ -21,42 +10,38 @@ class GUIhtml:
     def __init__(self, master):
 
         # Frame
-        master.title('Summer Sales')
+        master.title('Summer Sales Editor')
         master.resizable(False,False)
-        master.configure(background='#e1d8b9')
+        master.configure(background='#009999')
 
         # Colors, fonts, etc.
         self.style = ttk.Style()
-        self.style.configure('TFrame',background='#e1d8b9')
-        self.style.configure('TButton',background='#e1d8b9')
-        self.style.configure('TLabel',background='#e1d8b9', font=('Arial',11))
-        self.style.configure('Header.TLabel',background='#e1d8b9', font=('Arial',18,'bold'))
+        self.style.configure('TFrame',background='#009999')
+        self.style.configure('TButton',background='#009999')
+        self.style.configure('TLabel',background='#009999', font=('Arial',11))
+        self.style.configure('Header.TLabel',background='#009999',font=('Arial',18,'bold'))
 
         # Header
         self.frame_header = ttk.Frame(master)
         self.frame_header.pack()
 
         # Header content
-##        self.logo = PhotoImage(file='tour_logo.gif')
-##        ttk.Label(self.frame_header, image=self.logo).grid(row=0,column=0,rowspan=2)
-        ttk.Label(self.frame_header,text='Summer Sales',style='Header.TLabel').grid(row=0,column=1)
-        ttk.Label(self.frame_header, wraplength=381,
-                  text=("Welcome to the super easy content editor for the Summer Sales page!"
-                        " Enter your text below and press 'Submit'.")).grid(row=1,column=1)
+        ttk.Label(self.frame_header,text='Summer Sales',style='Header.TLabel',foreground='#ccffff').grid(row=0,column=1,pady=5)
+        ttk.Label(self.frame_header, wraplength=350,
+                  text=("Welcome to the super easy content editor for our Summer Sales page!"
+                        " Enter your text below and press 'Post'."),foreground='#ccffff').grid(row=1,column=1,pady=5)
 
         # Body 
         self.frame_content=ttk.Frame(master)
         self.frame_content.pack()
-        
-        # Body label
-        self.text_body=Text(self.frame_content,width=50,height=10, font=('Arial',10))
+        self.text_body=Text(self.frame_content,width=50,height=10)
 
         # Placement of textbox
-        self.text_body.grid(row=3,column=0,columnspan=2)
+        self.text_body.grid(row=3,column=0,columnspan=2,padx=5)
 
         # Buttons
-        ttk.Button(self.frame_content,text='Submit', command=self.submit).grid(row=4,column=0,padx=5, sticky='e')
-        ttk.Button(self.frame_content,text='Clear',command=self.clear).grid(row=4,column=1,padx=5,sticky='w')
+        ttk.Button(self.frame_content,text='Post', command=self.submit).grid(row=4,column=0,padx=5,pady=5, sticky='e')
+        ttk.Button(self.frame_content,text='Clear',command=self.clear).grid(row=4,column=1,padx=5,pady=5,sticky='w')
 
 #-------------------------FUNCTIONS-------------------------#
 
