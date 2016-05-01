5e251bd81d5228b29111e9384a06343ede283738
Nathan Reinauer <nathanreinauer@gmail.com>
2016-05-01 16:35:03 -0700

    Created variables to access the database.

--- HTML script/DBhtml.py
+++ HTML script/DBhtml.py
@@ -3,15 +3,18 @@ import sqlite3
 conn = sqlite3.connect('Content Database.db')
 c = conn.cursor()
 
+nameID = 2
+contentID = 3
+
 #-----RETRIEVE RECORD-----#
 
 def getName():
-    c.execute("SELECT Name FROM Content WHERE ID =1")
+    c.execute("SELECT Name FROM Content WHERE ID ={}".format(str(nameID)))
     return c.fetchall()
 
 
 def getContent():
-    c.execute("SELECT HTML FROM Content WHERE ID =5")
+    c.execute("SELECT HTML FROM Content WHERE ID ={}".format(str(contentID)))
     return c.fetchall()
 
 print(getName())
