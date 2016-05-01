3e04471f21298505ba4eb0189487ae326d3488e1
Nathan Reinauer <nathanreinauer@gmail.com>
2016-04-30 20:30:22 -0700

    Labeled every block with #
    To make it easier to see what I'm doing

--- HTML script/htmlGUI.py
+++ HTML script/htmlGUI.py
@@ -1,28 +1,31 @@
-#!/usr/bin/python3
-# feedback_template.py by Barron Stone
-# This is an exercise file from Python GUI Development with Tkinter on lynda.com
 
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 
-class Feedback:
+class GUIhtml:
+
+#-------------------------GUI-------------------------#
 
     def __init__(self, master):
 
-        master.title('Explore California Feedback')
+        # Frame
+        master.title('Summer Sales')
         master.resizable(False,False)
         master.configure(background='#e1d8b9')
 
+        # Colors, fonts, etc.
         self.style = ttk.Style()
         self.style.configure('TFrame',background='#e1d8b9')
         self.style.configure('TButton',background='#e1d8b9')
         self.style.configure('TLabel',background='#e1d8b9', font=('Arial',11))
         self.style.configure('Header.TLabel',background='#e1d8b9', font=('Arial',18,'bold'))
 
+        # Header
         self.frame_header = ttk.Frame(master)
         self.frame_header.pack()
 
+        # Header content
         self.logo = PhotoImage(file='tour_logo.gif')
         ttk.Label(self.frame_header, image=self.logo).grid(row=0,column=0,rowspan=2)
         ttk.Label(self.frame_header,text='Thanks for exploring!',style='Header.TLabel').grid(row=0,column=1)
@@ -30,41 +33,54 @@ class Feedback:
                   text=("We're glad you chose Explore California for your recent adventure!  "
                         "Please tell us what you thought about the 'Desert to Sea' tour.")).grid(row=1,column=1)
 
+        # Body 
         self.frame_content=ttk.Frame(master)
         self.frame_content.pack()
-
+        
+        # Body content (labels)
         ttk.Label(self.frame_content,text='Name:').grid(row=0,column=0, padx=5, sticky='sw')
         ttk.Label(self.frame_content,text='Email:').grid(row=0,column=1, padx=5, sticky='sw')    
         ttk.Label(self.frame_content,text='Comments:').grid(row=2,column=0,padx=5, sticky='sw')
 
+        # Body content (textboxes)
         self.entry_name=ttk.Entry(self.frame_content,width=24, font=('Arial',10))
         self.entry_email=ttk.Entry(self.frame_content,width=24, font=('Arial',10))
         self.text_comments=Text(self.frame_content,width=50,height=10, font=('Arial',10))
 
+        # Placement of textboxes
         self.entry_name.grid(row=1,column=0)
         self.entry_email.grid(row=1,column=1)
         self.text_comments.grid(row=3,column=0,columnspan=2)
 
+        # Buttons
         ttk.Button(self.frame_content,text='Submit', command=self.submit).grid(row=4,column=0,padx=5, sticky='e')
         ttk.Button(self.frame_content,text='Clear',command=self.clear).grid(row=4,column=1,padx=5,sticky='w')
 
+#-------------------------FUNCTIONS-------------------------#
+        
+        # Prints contents of textboxes to the shell on Submit click
     def submit(self):
         print('Name: {}'.format(self.entry_name.get()))
         print('Email: {}'.format(self.entry_email.get()))
         print('Comments: {}'.format(self.text_comments.get(1.0,'end')))
+        
+        # Clears textboxes after submitting
         self.clear()
+
+        # Confirmation of submission via dialog box
         messagebox.showinfo(title='Explore California Feedback',message='Comments submitted.')
 
-        
+        # Clears all textboxes on Clear click
     def clear(self):
         self.entry_name.delete(0,'end')
         self.entry_email.delete(0,'end')
         self.text_comments.delete(1.0,'end')
-                  
+        
+# Run program          
 def main():            
     
     root = Tk()
-    feedback = Feedback(root)
+    guihtml = GUIhtml(root)
     root.mainloop()
     
 if __name__ == "__main__": main()
