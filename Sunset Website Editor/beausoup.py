738afc5dd1ed46721c1fe34902d2dbb14bfc6a57
Nathan Reinauer <nathanreinauer@gmail.com>
2016-05-11 20:38:05 -0700

    Code to change "past movies" section on website
    Combine with Sunset.py later

---
+++ Sunset Website Editor/beausoup.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+url = "http://sunsettheatre.com/"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, "html5lib")
+
+font = soup.find("b", text="Past Movies:").find_next_sibling("font")
+for event in font.find_all("b", recursive=False):
+   event_date = event.previous_sibling.strip()
+   event_text = event.get_text(strip=True)
+   print(event_date, event_text)
